b9a23e17e48d3567d5f4b58b540995d159e3c465
Furkan Sahin <aliince.000000@gmail.com>
2023-07-25 01:20:36 +0300

    25.07.2023 - Ödev

---
+++ Delegates/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public delegate void MyDelegate();
+    public delegate void MyDelegate2(string text);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            //customerManager.SendMessage();
+            //customerManager.ShowAlert();
+
+
+            MyDelegate myDelegate = customerManager.SendMessage;
+            // Eklemek için;
+            myDelegate += customerManager.ShowAlert;
+            // Çıkartmak için;
+            myDelegate -= customerManager.SendMessage;
+
+            MyDelegate2 myDelegate2 = customerManager.SendMessage2;
+
+
+
+
+            myDelegate();
+            myDelegate2("Off");
+            Console.ReadLine();
+        }
+    }
+
+    class CustomerManager
+    {
+        public void SendMessage()
+        {
+            Console.WriteLine("Hello!");
+        }
+        public void ShowAlert()
+        {
+            Console.WriteLine("Be Careful!");
+        }
+        public void SendMessage2(string message)
+        {
+            Console.WriteLine(message);
+        }
+        public void ShowAlert2(string alert)
+        {
+            Console.WriteLine(alert);
+        }
+    }
+}
