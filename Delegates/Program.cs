5db4595cab973fc410bf89cf8369eff5c53a722c
Furkan Sahin <aliince.000000@gmail.com>
2023-07-25 01:48:52 +0300

    25.07.2023 - Ödev

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -8,6 +8,7 @@ namespace Delegates
 {
     public delegate void MyDelegate();
     public delegate void MyDelegate2(string text);
+    public delegate int MyDelegate3(int number1, int number2);
 
     class Program
     {
@@ -25,12 +26,22 @@ namespace Delegates
             myDelegate -= customerManager.SendMessage;
 
             MyDelegate2 myDelegate2 = customerManager.SendMessage2;
+            myDelegate2 += customerManager.ShowAlert2;
+
+            Matematik matematik = new Matematik();
+
+            MyDelegate3 myDelegate3 = matematik.Topla;
+            myDelegate3 += matematik.Carp;
+            int sonuc = myDelegate3(2, 5);
+            
+            Console.WriteLine(sonuc);
+            // Consol'da 'matematik.Topla' görmezden gelindi. Çünkü bu delege bir int döndürüyor ve...
+            // eğer bir delegede 'return type' var ise consola son verdiğimiz delegeyi ekrana yazacaktır.  
 
 
-
-
+            myDelegate2("Hello");
+            // myDelegate2("Dikkat et!");
             myDelegate();
-            myDelegate2("Off");
             Console.ReadLine();
         }
     }
@@ -52,6 +63,19 @@ namespace Delegates
         public void ShowAlert2(string alert)
         {
             Console.WriteLine(alert);
+        }
+    }
+
+    public class Matematik
+    {
+        public int Topla(int sayi1, int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+
+        public int Carp(int sayi1, int sayi2)
+        {
+            return sayi1 * sayi2;
         }
     }
 }
